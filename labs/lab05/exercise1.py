088ac416c5c50885e0eca178641a42c46f1e3c52
Hebe888-debug <hebechong74@gmail.com>
2026-08-03 09:21:29 +0800

    addexercise1

---
+++ labs/lab05/exercise1.py
@@ -0,0 +1,9 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+course_code=input("Enter your course code: ")
+
+print(f"Hello {name}, you are {age} years old and you are enrolled in {course_code}.")
+
+print(type(name))
+print(type(age))
+print(type(course_code))
