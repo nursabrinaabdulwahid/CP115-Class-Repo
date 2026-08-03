5ee562311b02626e733746ab72aa04fdcfd35533
Hebe888-debug <hebechong74@gmail.com>
2026-08-03 09:25:29 +0800

    add exercise 2 amd exercise 3

---
+++ labs/lab05/exercise2.py
@@ -0,0 +1,19 @@
+# Import entire modules
+import math
+import random
+import datetime
+
+# Using imported modules
+circle_area = math.pi * (5 ** 2)
+random_number = random.randint(1, 100)
+current_date = datetime.date.today()
+
+# Import specific functions from modules
+from math import sqrt, pow, sin, cos
+from random import choice, shuffle
+from datetime import datetime, timedelta
+
+# Using imported functions directly (no module prefix needed)
+square_root = sqrt(25)
+power_result = pow(2, 8)
+random_choice = choice(['apple', 'banana', 'cherry'])
