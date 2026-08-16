e03e466b648b13d7ef94e5099d487bdd026c86fc
Hebe888-debug <hebechong74@gmail.com>
2026-08-17 07:47:07 +0800

    update

---
+++ labs/lab06/exercise1/test/lab-test-2.py
@@ -0,0 +1,3 @@
+print ("Name: Ahmad Bin Abu\t\t\t\t\t\t Matric.No:MS2025123499\n\n\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\n\n" \
+"This is\t my\n\t second\n\t\tassignment\nI want 2x10 marks, which is 20 full marks")
+
